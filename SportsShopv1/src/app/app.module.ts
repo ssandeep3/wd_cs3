4af9b42870ecd7f26dd3f8467adcdb9ef840ce15
ssandeep3 <ssandeep3@users.noreply.github.com>
2019-05-01 09:47:31 +0530

    Add files via upload

---
+++ SportsShopv1/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StoreModule } from "./store/store.module";
+
+@NgModule({
+  // imports: [BrowserModule,StoreModule],
+  declarations: [
+    AppComponent
+  ], 
+  imports: [
+    BrowserModule,
+    AppRoutingModule, StoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
