7239f8ef948f9b21d4bdbf2cff66e260c38f8ee8
ssandeep3 <ssandeep3@users.noreply.github.com>
2019-05-02 08:46:07 +0530

    Add files via upload

---
+++ SportsShopV2/src/app/model/model.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { ProductRepository } from "./product.repository";
+import { StaticDataSource } from "./static.datasource";
+import { Cart } from "./cart.model";
+
+@NgModule({
+    providers: [ProductRepository, StaticDataSource, Cart]
+})
+export class ModelModule { }
